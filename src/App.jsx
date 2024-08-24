bd76a19a2554e98fc13871e06612bf0625f1dfe4
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-24 14:27:01 -0300

    React useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,41 @@ import Header from "./components/Header/Header";
 // import Contato from "./components/Contato/Contato";
 const Contato = React.lazy(() => import("./components/Contato/Contato"));
 
+function reducer(state, action){
+  switch(action){
+    case 'aumentar':
+      return state + 1;
+    case 'diminuir':
+      return state - 1;
+    default:
+      throw new Error();
+  }
+  // if(action === 'aumentar'){
+  //   return state + 1
+  // }
+  // if(action === 'diminuir'){
+  //   return state - 1
+  // }
+  // return new Error("Error action não existe")
+}
+
 const App = () => {
   const [ativo, setAtivo] = React.useState(false);
   const [contar, setContar] = React.useState(0)
+  const [state, dispatch] = React.useReducer(reducer, 0)
+
+
   return (
     <div>
       {/* Memo  */}
-      <Header/>
-      <button onClick={() => setContar(contar + 1)}>{contar}</button>
+      {/* <Header/>
+      <button onClick={() => setContar(contar + 1)}>{contar}</button> */}
+      {/* --- */}
+
+      {/* useReducer  */}
+      <button onClick={() => dispatch('aumentar')}>+</button>
+      <button onClick={() => dispatch('diminuir')}>-</button>
+      <p>{state}</p>
       {/* --- */}
 
       {/* PropTypes */}
