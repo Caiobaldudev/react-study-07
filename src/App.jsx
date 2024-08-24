0c2d07f1cde5bfaf922dedf398d6f66436cac446
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-24 12:28:49 -0300

    React Lazy & Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import Button from './components/Button/Button'
+import React from "react";
+import Button from "./components/Button/Button";
+import Contato from "./components/Contato/Contato";
+const Contato = React.lazy(() => import("./components/Contato/Contato"));
 
 const App = () => {
+  const [ativo, setAtivo] = React.useState(false);
   return (
     <div>
-      <h1>Olá mundo!</h1>
+      {/* PropTypes */}
       <Button width={300}>Clique Aqui</Button>
-    </div>
-  )
-}
+      {/* --- */}
 
-export default App
+      {/* Lazy & Suspense  */}
+      {/* Com o Lazy e Suspense podemos dividir o código da nossa aplicação. Assim o React só irá carregar certas partes do código, quando as mesmas forem necessárias. */}
+      {ativo && (
+        <React.Suspense fallback={<div>Carregando...</div>}>
+          <Contato />
+        </React.Suspense>
+      )}
+      <button onClick={() => setAtivo(true)}>Ativar</button>
+      {/* --- */}
+    </div>
+  );
+};
+
+export default App;
